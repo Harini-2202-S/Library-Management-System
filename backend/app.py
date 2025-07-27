face204e8002a69fb2e36382432acc5c2dbade91
Harini-2202-S <janaaharinis@gmail.com>
2025-07-27 19:49:25 +0530

    Backend fix

--- backend/app.py
+++ backend/app.py
@@ -294,5 +294,11 @@ def get_comments_by_title(title):
         cursor.close()
         conn.close()
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# testing deployment change
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
